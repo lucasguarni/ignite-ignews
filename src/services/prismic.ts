5104c461165c87761c5d4bf4b713e60ec87eccc1
Lucas Guarnieri <lucas.guarnieri.s@gmail.com>
2022-08-12 23:54:50 -0300

    feat: Cria página de detalhe do post

--- src/services/prismic.ts
+++ src/services/prismic.ts
@@ -1,6 +1,6 @@
 import * as Prismic from '@prismicio/client';
 
-export function getPrismictClient(req?: unknown) {
+export function getPrismicClient(req?: unknown) {
   const endpoint = Prismic.getEndpoint(process.env.PRISMIC_REPOSITORY_NAME)
   const client = Prismic.createClient(endpoint, {
     accessToken: process.env.PRISMIC_ACCESS_TOKEN
